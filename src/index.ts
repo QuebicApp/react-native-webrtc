40dc3b24f391a29a322453bec78ae0eca25f8f77
dtzxporter <dtzxporter@users.noreply.github.com>
2022-05-26 12:04:37 -0400

    Pull in changes to global

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,22 @@ export {
     registerGlobals
 };
 
+// React Native has a global object which acts like `window` in the browser.
+declare const global: {
+    navigator: {
+        mediaDevices: {
+            getUserMedia: (constraints: unknown) => Promise<MediaStream>,
+            getDisplayMedia: (constraints: unknown) => Promise<MediaStream>,
+            enumerateDevices: () => Promise<unknown>,
+        }
+    }
+    RTCPeerConnection: typeof RTCPeerConnection,
+    RTCIceCandidate: typeof RTCIceCandidate,
+    RTCSessionDescription: typeof RTCSessionDescription,
+    MediaStream: typeof MediaStream,
+    MediaStreamTrack: typeof MediaStreamTrack
+};
+
 function registerGlobals(): void {
     // Should not happen. React Native has a global navigator object.
     if (typeof global.navigator !== 'object') {
@@ -28,12 +44,12 @@ function registerGlobals(): void {
     }
 
     if (!global.navigator.mediaDevices) {
-        global.navigator.mediaDevices = {};
+        global.navigator.mediaDevices = {
+            getUserMedia: mediaDevices.getUserMedia.bind(mediaDevices),
+            getDisplayMedia: mediaDevices.getDisplayMedia.bind(mediaDevices),
+            enumerateDevices: mediaDevices.enumerateDevices.bind(mediaDevices)
+        };
     }
-
-    global.navigator.mediaDevices.getUserMedia = mediaDevices.getUserMedia.bind(mediaDevices);
-    global.navigator.mediaDevices.getDisplayMedia = mediaDevices.getDisplayMedia.bind(mediaDevices);
-    global.navigator.mediaDevices.enumerateDevices = mediaDevices.enumerateDevices.bind(mediaDevices);
 
     global.RTCPeerConnection = RTCPeerConnection;
     global.RTCIceCandidate = RTCIceCandidate;
